0f2e8a36595069a806a19b6b6873283624d79e49
fallboy <fallboy@163.com>
2023-11-16 01:58:42 +0800

    openai test

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from openai import OpenAI
 
 app = Flask(__name__)
 
@@ -9,3 +10,19 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+
+@app.route('/openai')
+def about():
+    client = OpenAI()
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+            {"role": "user", "content": "你好"}
+        ]
+    )
+    print("124")
+    print(completion.choices[0].message)
+
+    return 'openai'
